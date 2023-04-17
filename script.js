13fae8dc169a7cafe3b5de171503d08d1cba776b
Tulasi Latha Gangadhari <65495688+TulasiLatha@users.noreply.github.com>
2023-04-17 16:54:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
 // your code here
+	let n1=0,n2=1;
+	let n3=0;
+	console.log(n1+" "+n2);
+	for(let i=2;i<num;i++){
+		n3=n1+n2;
+		console.log(n3+" ");
+		n1=n2;
+		n2=n3;
+	}
 }
 
 module.exports = fibonacci;
